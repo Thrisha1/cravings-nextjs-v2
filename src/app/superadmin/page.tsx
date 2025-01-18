e761f5ddd6bae44dcbc146fd60fc1ab0e6a6d552
ab-h-i-n <abhinshelly03@gmail.com>
2025-01-18 22:56:10 +0530

    In super admin dashboard a table for offers and no of claims

--- src/app/superadmin/page.tsx
+++ src/app/superadmin/page.tsx
@@ -1,8 +1,55 @@
-import SuperAdmin from "@/screens/SuperAdmin";
+import OfferDetails from "@/components/superAdmin/OfferDetails";
+import PartnerVerification from "@/components/superAdmin/PartnerVerification";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import { PageProps } from "../../../.next/types/app/superadmin/page";
 
-const page = () => {
-  return <SuperAdmin />;
+const page = async ({ searchParams }: PageProps) => {
+  const page = (await searchParams).page;
+  const pages = [
+    {
+      name: "Partner Verification",
+      component: <PartnerVerification />,
+      id: "partner-verification",
+    },
+    {
+      name: "Offer Details",
+      component: <OfferDetails />,
+      id: "offer-Details",
+    },
+  ];
+
+  if (page) {
+    const selectedPage = pages.find((p) => p.id === page);
+    return (
+      <main className="px-3 py-5 sm:px-[7.5%] bg-[#FFF7EC] min-h-screen">
+        <h1 className="text-2xl lg:text-4xl font-bold mb-5">
+          {selectedPage?.name}
+        </h1>
+        {selectedPage?.component}
+      </main>
+    );
+  } else {
+    return (
+      <main className="px-3 py-5 sm:px-[7.5%] bg-[#FFF7EC] min-h-screen">
+        <h1 className="text-2xl lg:text-4xl font-bold">Dashboard</h1>
+
+        <div className="mt-5">
+          {pages.map((p) => (
+            <Link
+              className="font-medium p-4 mt-2 rounded border-2 border-[#ffba79]/20 bg-[#fffefd] flex items-center justify-between"
+              href={`?page=${p.id}`}
+              key={p.name}
+            >
+              {p.name}
+              <ChevronRight size={24} className="text-orange-600" />
+            </Link>
+          ))}
+        </div>
+      </main>
+    );
+  }
 };
 
 export default page;
---
+++ src/components/superAdmin/OfferDetails.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { db } from "@/lib/firebase";
+import { Offer } from "@/store/offerStore";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+const OfferDetails = () => {
+  const [offers, setOffers] = useState<Offer[]>();
+
+  const fetchOffers = async () => {
+    try {
+      const offersColleciton = await collection(db, "offers");
+      const offersSnapshot = await getDocs(offersColleciton);
+      const offersData: Offer[] = [];
+      offersSnapshot.forEach((doc) => {
+        offersData.push({ id: doc.id, ...doc.data() } as Offer);
+      });
+
+      offersData.sort((a, b) => {
+        return b.enquiries - a.enquiries;
+      });
+
+      setOffers(offersData);
+    } catch (error) {
+      console.error("Error fetching offers:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOffers();
+  }, []);
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Dish Name</TableHead>
+          <TableHead>Enquires</TableHead>
+          <TableHead>Hotel Name</TableHead>
+          <TableHead>Has Expired</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {offers?.map((offer) => (
+          <TableRow key={offer.id}>
+            <TableCell>{offer.dishName}</TableCell>
+            <TableCell>{offer.enquiries}</TableCell>
+            <TableCell>{offer.hotelName}</TableCell>
+            <TableCell>
+              {new Date(offer.toTime) < new Date() ? "Yes" : "No"}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default OfferDetails;
