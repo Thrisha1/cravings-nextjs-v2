e3fb07e963bb763a17a41011ae9778e304dfc8bc
ab-h-i-n <abhinshelly03@gmail.com>
2025-01-23 20:23:38 +0530

    changed offers card loading size

--- src/components/OfferCardsLoading.tsx
+++ src/components/OfferCardsLoading.tsx
@@ -8,7 +8,7 @@ const OfferCardsLoading = () => {
 
       <div className="grid gap-2 gap-y-5 grid-cols-2 md:grid-cols-4 md:gap-x-5 md:gap-y-10 mt-5 ">
         {Array.from({ length: 4 }).map((_, i) => (
-          <div key={'loading' + i} className="bg-white w-full h-[341.600px] rounded-xl grid grid-rows-2 overflow-hidden animate-pulse" >
+          <div key={'loading' + i} className="bg-white w-full h-[277.6px] rounded-xl grid grid-rows-2 overflow-hidden animate-pulse" >
             <div className="bg-gray-200 w-full animate-pulse delay-75" />
             <div className="py-5 px-2 h-fit w-full grid gap-3">
 
