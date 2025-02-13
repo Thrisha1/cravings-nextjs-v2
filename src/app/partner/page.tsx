9ecaa2134d7b2f13a9397658be11c2f6706442c0
Thrisha1 <thrishakannan25@gmail.com>
2025-02-13 09:35:04 +0530

    partner with us page true bug fixed and redict added for IOS devices

--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -6,7 +6,7 @@ export default function PartnerPage() {
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
       <div className="max-w-[425px] w-full">
-        <PartnerDialog isPage={true} />
+        <PartnerDialog />
       </div>
     </div>
   );
