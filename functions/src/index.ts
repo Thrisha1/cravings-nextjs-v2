121d9379814f1d522843d144fae94dc41b3343f1
ab-h-i-n <abhinshelly03@gmail.com>
2025-02-16 13:31:02 +0530

    fix build

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,1 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-admin.initializeApp();
-
-export const createCustomToken = functions.https.onCall(async (data, context) => {
-  try {
-    const customToken = await admin.auth().createCustomToken(data.uid);
-    return customToken;
-  } catch (error) {
-    throw new functions.https.HttpsError('internal', 'Error creating custom token');
-  }
-}); 
+ 
