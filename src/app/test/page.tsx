49650d61472a6b8f4368f7fd090dbaf850414bca
ab-h-i-n <abhinshelly03@gmail.com>
2025-03-02 18:12:42 +0530

    removed all the users

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,31 +1,44 @@
 "use client";
-// import Link from "next/link";
-import { useRouter } from "next/navigation";
-// import { Button } from "@/components/ui/button";
-// import { Link } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
+// import { collection, query, where, getDocs } from "firebase/firestore";
+// import { db } from "@/lib/firebase";
 
 const TestPage = () => {
-  const [upiLink, setUpiLink] = useState("upi://pay?pa=6238969297@ptyes&pn=MUHAMMED%20MUSTHAFA%20AMEEN%20%20N%20M");
-  const router = useRouter();
+  // const [users, setUsers] = useState<any[]>([]);
 
-  const onClickFn = () => {
-    router.push(upiLink);
-  }
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const usersRef = collection(db, "users");
+  //     const q = query(usersRef, where("role", "==", "user"));
+  //     const querySnapshot = await getDocs(q);
+  //     const usersList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  //     setUsers(usersList);
+      
+  //     // Delete all users from the users list in Firebase
+  //     // await Promise.all(usersList.map(user => deleteDoc(doc(usersRef, user.id))));
+  //   };
+
+  //   fetchUsers();
+  // }, []);
+
+  // const downloadJson = () => {
+  //   const json = JSON.stringify(users, null, 2);
+  //   const blob = new Blob([json], { type: "application/json" });
+  //   const url = URL.createObjectURL(blob);
+  //   const a = document.createElement("a");
+  //   a.href = url;
+  //   a.download = "UsersList3/2/25.json";
+  //   document.body.appendChild(a);
+  //   a.click();
+  //   document.body.removeChild(a);
+  //   URL.revokeObjectURL(url);
+  // };
 
   return (
     <div>
-      <div>Test Link : </div>
-      <input 
-        type="text" 
-        value={upiLink} 
-        onChange={(e) => setUpiLink(e.target.value)} 
-        placeholder="Enter UPI link" 
-        className="border p-2 mb-4 bg-white"
-      />
-
-      <div>Click :</div>
-      <div onClick={onClickFn}>{upiLink}</div>
+      {/* <h1>Users with Role 'User'</h1>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <button onClick={downloadJson}>Download JSON</button> */}
     </div>
   );
 };
