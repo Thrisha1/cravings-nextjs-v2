ac95c7dd4649af816f548579fb3648da5c316e4b
ab-h-i-n <abhinshelly03@gmail.com>
2025-04-25 16:17:07 +0530

    added error pages

---
+++ src/app/qrScan/error.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { UtensilsCrossed } from "lucide-react";
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="grid place-content-center w-full min-h-[90vh] opacity-70">
+      <UtensilsCrossed className="text-orange-600 h-40 w-40 justify-self-center" />
+      <h1 className="text-center mt-2 font-bold text-xl text-orange-600">
+        Qr not found!
+      </h1>
+    </div>
+  );
+};
+
+export default Error;
