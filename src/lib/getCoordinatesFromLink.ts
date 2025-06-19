c4a7eae7fe8a780ac0575a6331834c4212263e2a
ab-h-i-n <abhinshelly03@gmail.com>
2025-06-19 20:59:37 +0530

    added locaiton convert try-catch

--- src/lib/getCoordinatesFromLink.ts
+++ src/lib/getCoordinatesFromLink.ts
@@ -1,21 +1,26 @@
 'use server';
 
 export async function getCoordinatesFromLink(link: string) {
-  const coordinates = await fetch(
-    `${process.env.NEXT_PUBLIC_FIREBASE_BACKEND_URL}/api/convert-coordinate`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ url: link }),
-    }
-  );
-
-  const coords =  await coordinates.json();
-
-  console.log(`Fetching coordinates ` , coords);
+  try{
+    const coordinates = await fetch(
+      `${process.env.NEXT_PUBLIC_FIREBASE_BACKEND_URL}/api/convert-coordinate`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: link }),
+      }
+    );
   
-
-  return  coords;  
+    const coords =  await coordinates.json();
+  
+    console.log(`Fetching coordinates ` , coords);
+    
+  
+    return  coords;  
+  }catch(error){
+    console.error("Error in getCoordinatesFromLink:", error);
+    throw error;
+  }
 }
