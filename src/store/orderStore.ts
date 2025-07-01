4731deeca077812c44590ce8e64161d4fa3ab418
afthaabb <afthabnaydin@gmail.com>
2025-07-01 19:24:33 +0530

    user note

--- src/store/orderStore.ts
+++ src/store/orderStore.ts
@@ -164,7 +164,8 @@ interface OrderState {
           charge_type?: string;
         }[]
       | null,
-    deliveryCharge?: number
+    deliveryCharge?: number,
+    notes?: string
   ) => Promise<Order | null>;
   getCurrentOrder: () => HotelOrderState;
   fetchOrderOfPartner: (partnerId: string) => Promise<Order[] | null>;
@@ -827,10 +828,10 @@ const useOrderStore = create(
 
      
       placeOrder: async (
-        hotelData,
-        tableNumber,
-        qrId,
-        gstIncluded,
+        hotelData: HotelData,
+        tableNumber?: number,
+        qrId?: string,
+        gstIncluded?: number,
         extraCharges?:
           | {
               name: string;
@@ -838,7 +839,8 @@ const useOrderStore = create(
               charge_type?: string;
             }[]
           | null,
-        deliveryCharge?: number
+        deliveryCharge?: number,
+        notes?: string
       ) => {
         try {
           const state = get();
@@ -944,6 +946,7 @@ const useOrderStore = create(
                     ],
                   }
                 : null,
+            notes: notes || null,
           });
 
           if (orderResponse.errors || !orderResponse?.insert_orders_one?.id) {
