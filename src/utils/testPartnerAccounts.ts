2bc1bec325a68b7274c95a508f2626b19ffd1770
ab-h-i-n <abhinshelly03@gmail.com>
2025-07-26 21:07:00 +0530

    added test partner restriction on sending whatsapp message forr offers

---
+++ src/utils/testPartnerAccounts.ts
@@ -0,0 +1,6 @@
+ const TEST_PARTNERS = [
+    "397fd024-3044-40a7-be52-c82a03f07cbb", //abhinshelly03@gmail.com
+    "373a15f9-9c58-4e34-ae07-b272e578928f" //partner1@gmail.com
+ ]
+
+export default TEST_PARTNERS;
