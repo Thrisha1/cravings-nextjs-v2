2d2db5bfc671606042b5414256043650d68e1098
ab-h-i-n <abhinshelly03@gmail.com>
2025-07-26 21:20:50 +0530

    updated test accounts

--- src/utils/testPartnerAccounts.ts
+++ src/utils/testPartnerAccounts.ts
@@ -1,6 +1,7 @@
  const TEST_PARTNERS = [
     "397fd024-3044-40a7-be52-c82a03f07cbb", //abhinshelly03@gmail.com
-    "373a15f9-9c58-4e34-ae07-b272e578928f" //partner1@gmail.com
+    "373a15f9-9c58-4e34-ae07-b272e578928f", //partner1@gmail.com
+    "3afca6ce-8669-4301-89b6-dc5331fe94c5" //test@gmail.com
  ]
 
 export default TEST_PARTNERS;
