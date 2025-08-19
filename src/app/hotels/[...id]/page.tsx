65f692b366f15d562c6316a0b60768d9910b716c
ab-h-i-n <abhinshelly03@gmail.com>
2025-08-19 19:19:30 +0530

    added test page

--- src/app/hotels/[...id]/page.tsx
+++ src/app/hotels/[...id]/page.tsx
@@ -49,7 +49,6 @@ export async function generateMetadata({
   );
 
   const hotel = await getHotelData(hotelId);
-  console.log("partnerdata",hotel);
 
   if (!hotel) {
     throw new Error("Hotel not found");
